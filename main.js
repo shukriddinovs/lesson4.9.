9573ef1565e083af6d1746eb4932c0633f0872cc
shukriddinovs <sshukriddinov524@gmail.com>
2024-05-09 14:13:41 +0500

    {.}

---
+++ main.js
@@ -0,0 +1,33 @@
+// const array = [1,2,3,4,5,6,7,8,9,10];
+// const odd = [];
+// const juft = [];
+// for(const element of array){
+//     if (element % 2 === 1) {
+// odd.push(element)
+//     }else{
+//         juft.push(element)
+//     }
+// }
+// console.log(odd);
+// console.log(juft);
+
+const array = ["non", "asal", "banan", "olma", "aziza", "Gul", "mashina"];
+const nharfibor = []
+const nharfiyoq = []
+const check = array.includes;
+for (const element of array) {
+
+    if (array === nharfibor.includes)
+        console.log(element + "bu sozda n harfi mavjud ");
+    
+    else {
+        console.log( element + "bu sozda n harfi mavjud emas");
+    }
+     
+}
+
+
+
+console.log(nharfibor);
+console.log(nharfiyoq);
+
